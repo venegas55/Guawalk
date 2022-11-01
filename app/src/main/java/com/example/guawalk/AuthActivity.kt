15aa579d8e3d03eb61ac12c054e38263f0466f08
Juan <venegas55@me.com>
2022-10-31 23:45:35 -0600

    Buttons are functional Study Case, make view-bindings true stock on all projects firebase connection also works

--- app/src/main/java/com/example/guawalk/AuthActivity.kt
+++ app/src/main/java/com/example/guawalk/AuthActivity.kt
@@ -1,8 +1,14 @@
 package com.example.guawalk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +25,68 @@ class AuthActivity : AppCompatActivity() {
         //setup del Auth
         setup()
 
-    }
-    private fun setup() {
-        
 
     }
-}
+    // Alerta de autenticacion fallida
+    private fun showAlert() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+    //Logica para cambiar de actividad
+    private fun showHome (email: String, provider: ProviderType) {
+
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+
+    }
+
+    private fun setup() {
+        title = "LogIn"
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        val logInButton = findViewById<Button>(R.id.logInButton)
+        val emailTB = findViewById<EditText>(R.id.emailTB)
+        val passwordTB = findViewById<EditText>(R.id.passwordTB)
+        //Logica para el boton registrar
+        signUpButton.setOnClickListener {
+            if (emailTB.text.isNotEmpty() && passwordTB.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailTB.text.toString(), passwordTB.text.toString()).addOnCompleteListener{
+
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+        logInButton.setOnClickListener {
+            if (emailTB.text.isNotEmpty() && passwordTB.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailTB.text.toString(), passwordTB.text.toString()).addOnCompleteListener{
+
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+        }
+
+
+
+
+
+
+    }
--- app/src/main/java/com/example/guawalk/HomeActivity.kt
+++ app/src/main/java/com/example/guawalk/HomeActivity.kt
@@ -2,10 +2,41 @@ package com.example.guawalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    BASIC
+}
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Setup
+        val bundle: Bundle? = intent.extras
+        val email:String? = bundle?.getString("email")
+        val provider:String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String) {
+        title = "home"
+        val emailLabel = findViewById<TextView>(R.id.emailTV)
+        val providerLabel = findViewById<TextView>(R.id.providerTV)
+        val logOutButton = findViewById<Button>(R.id.logOutButton)
+        emailLabel.text = email
+        providerLabel.text = provider
+
+        logOutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+
+        }
+
+
     }
 }
